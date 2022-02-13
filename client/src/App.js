e4b5d9e780ac8e1bc4ba5f8116a756a226c3fa52
avict <avictorg@gmail.com>
2022-02-13 19:22:33 -0300

    Ambiente Compra pré-feito

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,85 @@
-import './App.css';
-import { BrowserRouter as Router, Link, Route, NavLink, Routes } from 'react-router-dom'
-
-
-import Insert from './rotas/Insert';
-import Drop from './rotas/Drop';
-import Read from './rotas/Read';
-import Update from './rotas/Update';
-
-
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  NavLink,
+  Routes,
+} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { NavDropdown, Nav, Navbar, Container } from "react-bootstrap";
+import Insert from "./rotas/Insert";
+import Drop from "./rotas/Drop";
+import Read from "./rotas/Read";
+import Update from "./rotas/Update";
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+
+    script.src =
+      "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js";
+    script.integrity =
+      "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p";
+    script.crossOrigin = "anonymous";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <Router>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"></link>
-      <div className='App'>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="container-fluid">
-            <a class="navbar-brand">CRUD</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link" href="/Insert">Create</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/Read">Read</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/Update">Update</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/Drop">Delete</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-            <Routes>
-                <Route path="/insert" element={<Insert />} />
-                <Route path="/read" element={<Read />} />
-                <Route path="/update" element={<Update />} />
-                <Route path="/drop" element={<Drop />} />
-            </Routes>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+      ></link>
+      <div className="App">
+        <Navbar variant="dark" bg="dark" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="/">CRUD</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-dark-example" />
+            <Navbar.Collapse id="navbar-dark-example">
+              <Nav>
+                <NavDropdown
+                  id="nav-dropdown-dark-example"
+                  title="Produto"
+                  menuVariant="dark"
+                >
+                  <NavDropdown.Item href="/Insert">Create</NavDropdown.Item>
+                  <NavDropdown.Item href="/Read">Read</NavDropdown.Item>
+                  <NavDropdown.Item href="/Update">Update</NavDropdown.Item>
+                  <NavDropdown.Item href="/Drop">Delete</NavDropdown.Item>
+                </NavDropdown>
+
+                <NavDropdown
+                  id="nav-dropdown-dark-example"
+                  title="Compra"
+                  menuVariant="dark"
+                >
+                  <NavDropdown.Item href="/InsertCompra">
+                    Create
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/ReadCompra">Read</NavDropdown.Item>
+                  <NavDropdown.Item href="/UpdateCompra">
+                    Update
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/DropCompra">Delete</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Routes>
+          <Route path="/insert" element={<Insert />} />
+          <Route path="/read" element={<Read />} />
+          <Route path="/update" element={<Update />} />
+          <Route path="/drop" element={<Drop />} />
+        </Routes>
       </div>
     </Router>
   );
